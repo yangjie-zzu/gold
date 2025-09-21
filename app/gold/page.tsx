d857c4fa9c88aad5af314acdd8e50e40e9f05547
yangjie-zzu <1342986150@qq.com>
2025-09-21 21:58:21 +0800

    代码优化

--- app/gold/page.tsx
+++ app/gold/page.tsx
@@ -3,12 +3,16 @@ import { prisma } from "@/prisma/prismaClient";
 import { LineChart } from "./LineChart";
 
 export default async function Gold() {
-    const goldPrices = await prisma.gold_price.findMany();
+    const fetchList = async () => {
+        "use server"
+        return await prisma.gold_price.findMany();
+    }
+    const goldPrices = await fetchList();
     return (
         <div style={{ width: '100vw', height: '100vh', overflow: 'auto', paddingRight: 1, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
             <LineChart initData={goldPrices} getDataFunc={async () => {
                 "use server"
-                return await prisma.gold_price.findMany();
+                return await fetchList();
             }}/>
         </div>
     )
