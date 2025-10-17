f3c70c4fb1e6ab1b41c551dfed4fc2dadc810a46
yangjie-zzu <1342986150@qq.com>
2025-10-17 19:03:19 +0800

    adapter

--- prisma/prismaClient.ts
+++ prisma/prismaClient.ts
@@ -1,4 +1,9 @@
 
 import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
 
-export const prisma = new PrismaClient();
+export const prisma = new PrismaClient({
+    adapter: new PrismaPg({
+        connectionString: process.env.DATABASE_URL!,
+    }),
+});
